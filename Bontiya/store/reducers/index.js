2d980ede8bc939fe7656ef0774d3cd7316ea0eb1
dzakki <dzakkiaz7@gmail.com>
2020-02-22 19:12:53 +0700

    inital redux

---
+++ Bontiya/store/reducers/index.js
@@ -0,0 +1,41 @@
+import {  combineReducers } from 'redux';
+import { ISLOGIN, GENERAL_ONLOAD, ERRORS, SUCCESS } from '../actionTypes';
+
+const initalState = {
+    isLogged: null,
+    generalOnload: false,
+    errors: null,
+    success: null
+}
+ 
+function general(state = initalState, action) {
+    switch (action.type) {
+        case ISLOGIN:
+            return {
+                ...state,
+                generalOnload: false,
+                isLogged: action.data
+            }
+        case GENERAL_ONLOAD:
+            return {
+                ...state,
+                generalOnload: true
+            }
+        case ERRORS: 
+            return {
+                ...state,
+                errors: action.data
+            }
+        case SUCCESS: 
+            return {
+                ...state,
+                success: action.data
+            }
+        default:
+            break;
+    }
+}
+
+export default combineReducers({
+    general
+})
